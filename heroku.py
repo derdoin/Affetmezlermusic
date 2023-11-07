4b2f45ca6730347573366487f286ffabb7693e80
derdoin <114993109+derdoin@users.noreply.github.com>
2023-11-07 11:48:23 +0300

    Delete heroku.py

--- heroku.py
+++
@@ -1,8 +0,0 @@
-$ heroku login
-$ cd my-project/
-$ git init
-$ heroku git:remote -a menrosebot
-  $ git add .
-$ git commit -am "make it better"
-$ git push heroku master
-$ heroku git:remote -a menrosebot
